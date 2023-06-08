4e75c6a35feadeeb7d03a0ee5d0f8fff0c6af649
Mohammed Abdul Aziz <cloudaziz@gmail.com>
2023-06-08 16:34:09 +0600

    adding functionality in the script.js

--- script.js
+++ script.js
@@ -1,11 +1,78 @@
-const animals = [
-  { name: 'karim', species: 'rabbit' },
-  { name: 'rahim', species: 'dog' },
-  { name: 'ashik', species: 'dog' },
-  { name: 'jabbar', species: 'rabbit' },
-];
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
 
-let dogs = animals.filter((animal) => {
-  return animal.species == 'dog';
+openModalButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
 });
-console.log(dogs);
+closeModalButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const modal = button.closest('.modal');
+    closeModal(modal);
+  });
+});
+
+function openModal(modal) {
+  if (modal == null) {
+    return;
+  }
+  modal.classList.add('active');
+  overlay.classList.add('active');
+}
+function closeModal(modal) {
+  if (modal == null) {
+    return;
+  }
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active');
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
+});
+
+window.addEventListener('keydown', (e) => {
+  //   console.log(e.keyCode);
+  const modals = document.querySelectorAll('.modal.active');
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
+});
+
+/* const openModalButton = document.querySelector('#openModalBtn');
+const closeModalButton = document.querySelector('.close-button');
+
+const modal = document.querySelector('#modal');
+const overlay = document.getElementById('overlay');
+
+openModalButton.addEventListener('click', () => {
+  modal.classList.add('active');
+  overlay.classList.add('active');
+});
+
+closeModalButton.addEventListener('click', () => {
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+});
+
+overlay.addEventListener('click', () => {
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}); */
+
+/* const modal = document.querySelector('#modal');
+// const overlay = document.getElementById('overlay');
+
+window.addEventListener('keydown', (e) => {
+  //   console.log(e.keyCode);
+  if (e.keyCode == 27) {
+    modal.classList.remove('active');
+    overlay.classList.remove('active');ß
+  }
+}); */
